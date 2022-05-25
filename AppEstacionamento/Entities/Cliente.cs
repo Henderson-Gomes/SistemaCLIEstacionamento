e5f066a13cc8bcfe4816242d0190f2387b599b38
Henderson gomes da silva <HendersonGomesDaSilva@outlook.com>
2022-05-25 10:53:15 -0300

    Primeiro Commit

---
+++ AppEstacionamento/Entities/Cliente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppEstacionamento.Entities
+{
+    internal class Cliente 
+    {
+        public  int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        
+        
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Placa { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+        public static List<Cliente> ListaCarros { get; set; } = new List<Cliente> ();
+    
+
+        
+
+        public Cliente(string nome, string cPF, string modelo, string marca, string placa, DateTime entrada,int id)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Modelo = modelo;
+            Marca = marca;
+            Placa = placa;
+            Entrada = entrada;
+            Id = id;
+
+            
+        }
+
+        public void MarcarEntrada(Cliente c)
+        {
+            ListaCarros.Add(c);
+        }
+        public void MarcarSaida(int id) 
+        {
+            DateTime d = new DateTime();
+            d = DateTime.Now;
+            foreach (Cliente c in ListaCarros)
+            {
+                if (c.Id == id)
+                {
+                    c.Saida = d;
+                 
+                }
+            }
+            {
+
+            }
+        }
+        public void Listar()
+        {
+            Console.WriteLine();
+            Console.WriteLine("                         LISTA DE CLIENTES                             ");
+            foreach(Cliente c in ListaCarros)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine("-----------------------------------------------------------------------");
+            }
+        }
+        
+        public override string ToString()
+        {
+            return $"[Nome: {Nome}] [CPF: {CPF}] [Modelo: {Modelo}] [Marca: {Marca}] [Placa: {Placa}] [Entrada: {Entrada}] [Saida: {Saida}]";
+        }
+    }
+}
---
+++ AppEstacionamento/Program.cs
@@ -0,0 +1,51 @@
+﻿using AppEstacionamento.Entities;
+
+
+int id = 0;
+Cliente cliente = new Cliente("Henderson", "442.063.338-74", "HB20", "Honda", "HEZ76", DateTime.Now,id++);
+cliente.MarcarEntrada(cliente);
+cliente = new Cliente("Gizelli", "442.872.112-74", "RC22", "GIP", "YUN23", DateTime.Now,id++);
+cliente.MarcarEntrada(cliente);
+
+DesingMenu();
+
+
+ void DesingMenu()
+{
+    int resp = 0;
+    do
+    {
+
+        Console.WriteLine("                    MENU                    ");
+        Console.WriteLine("+------------------------------------------+");
+        Console.WriteLine("[1] Lista dos Carros");
+        Console.WriteLine("[2] Marcar Entrada");
+        Console.WriteLine("[3] Marcar Saida");
+        Console.WriteLine("[4] Sair ");
+        Console.WriteLine("+------------------------------------------+");
+        resp = Convert.ToInt32(Console.ReadLine()); 
+
+        switch (resp)
+        {
+            case 1:
+                cliente.Listar();
+                DesingMenu();
+
+                break;
+            case 2:
+                cliente = new Cliente("Hathos", "332.322.999-32", "YAMAHA", "YINLDAI", "VS288", DateTime.Now, 3);
+                cliente.MarcarEntrada(cliente);
+
+                break;
+            case 3:
+                Console.WriteLine("ID do cliente: ");
+                int id = Convert.ToInt32(Console.Read());
+                cliente.MarcarSaida(id);
+                DesingMenu(); 
+                break;
+
+        }
+    } while (resp != 4);
+    
+    
+}
